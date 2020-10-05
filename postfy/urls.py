db2a7f080c5e312df35558a4585b38318d56e4da
Minab Ibrahim <64673241+NswMina@users.noreply.github.com>
2020-10-05 02:52:22 +0200

    Update urls.py

--- postfy/urls.py
+++ postfy/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('register', include('users.registerurl')),
     path('profile', include('users.profileurl')),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html',redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done'),
